b537ceeab837082cfd20f80d2f8e6c9c0c0029f0
Jay Suratwala <81503183+JaySuratwala@users.noreply.github.com>
2025-02-08 21:09:18 +0000

    button layout changes

--- Website/practicum-d/app/buttons.tsx
+++ Website/practicum-d/app/buttons.tsx
@@ -9,13 +9,13 @@ export function TailwindcssButtons() {
     return (
         <div className="pb-40 px-4 w-full">
             <Toaster position="top-center" />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full  max-w-7xl mx-auto gap-10">
+            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full  max-w-7xl mx-auto gap-10"> */}
                 {buttons.map((button, idx) => (
                 <ButtonsCard key={idx}>
                     {button.component}
                 </ButtonsCard>
                 ))}
-            </div>
+            {/* </div> */}
         </div>
     );
 }
@@ -26,15 +26,6 @@ export const buttons = [
     component: (
       <button className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear">
         Next.js Blue
-      </button>
-    ),
-  },
-  {
-    name: "Next.js White",
-    description: "Next.js White button for your website",
-    component: (
-      <button className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2 bg-[#fff] text-[#696969] rounded-md font-light transition duration-200 ease-linear">
-        Next White
       </button>
     ),
   },
