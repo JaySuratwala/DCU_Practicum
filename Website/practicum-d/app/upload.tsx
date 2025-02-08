5eb6764bdf6156caa7374a29ff1f0d7dc593bd10
Jay Suratwala <81503183+JaySuratwala@users.noreply.github.com>
2025-02-08 20:56:16 +0000

    submit button

--- Website/practicum-d/app/upload.tsx
+++ Website/practicum-d/app/upload.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { FileUpload } from "@/components/upload";
+import { TailwindcssButtons } from "./buttons";
 
 export function FileUploadDemo() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -13,6 +14,7 @@ export function FileUploadDemo() {
   return (
     <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
       <FileUpload onChange={handleFileUpload} />
+      <TailwindcssButtons></TailwindcssButtons>
     </div>
   );
 }
